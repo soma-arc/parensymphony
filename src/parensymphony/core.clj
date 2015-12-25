fb26c3d78382b05c9f63a36d4e2b94ae1ae538fb
soma_arc <somaarcr@gmail.com>
2015-12-25 20:47:16 +0900

    Implement multiple code editing

--- src/parensymphony/core.clj
+++ src/parensymphony/core.clj
@@ -32,7 +32,6 @@
         reverb (free-verb clp 0.4 0.8 0.2)]
     (* amp (env-gen (perc 0.0001 dur) :action FREE) reverb)))
 
-
 (def prog-55 [(chord :A2 :minor)
               (chord :F2 :major)
               (chord :G2 :major)
@@ -53,73 +52,87 @@
 (defn inc-chord-index [index]
   (rem (inc index) 4))
 
+(defn make-code-unit [x y]
+  {:start-x x :start-y y
+   :text-size 40 :text-color [255]
+   :cursor-color [0 255 0]
+   :cursor-index 0 :code ""})
+
 (defn setup []
   (q/smooth)
   (q/background 0)
   {:pressed-key ""
    :scale-index 0 :scale-root :E3 :scale-name :phrygian
    :chord-index 0 :chord-root :C3 :chord-name :major
-   :text-size 20
-   :cursor-index 0 :code ""
+   :code-unit-index 0 :code-list [(make-code-unit 20 100) (make-code-unit 20 200)]
    :pressing-ctr? false :pressing-alt? false})
 
-(defn display-cursor [{:keys [cursor-index code] :as state}]
-  (let [x (+ 20 (q/text-width (subs code 0 cursor-index)))
-        y-start 100
-        y-end (- y-start 20)]
-    (q/stroke 0 255 0)
-    (q/line x y-start x y-end)
-    (q/line x y-start x y-end)))
+(defn display-cursor [{:keys [start-x start-y text-size cursor-index code cursor-color]}]
+  (let [c (map count (clojure.string/split code #"\n"))
+        [cur-line-index line-count str-num-to-cursor line-breaks]
+        (loop [i 0 acc 0 line-breaks ""]
+          (if (and (< i (count c)) (> (- cursor-index i) (+ (nth c i) acc)))
+            (recur (inc i) (+ acc (nth c i)) (str "\n" line-breaks))
+            [(- cursor-index acc i) i acc line-breaks]))
+        x (+ start-x (q/text-width (subs code (+ str-num-to-cursor line-count) cursor-index)))]
+    (apply q/fill cursor-color)
+    (q/text (str line-breaks "|") (- x (/ (q/text-width "|") 2)) start-y)))
+(start)
+(defn display-code [{:keys [start-x start-y code text-size text-color]}]
+  (q/text-size text-size)
+  (apply q/fill text-color)
+  (q/text code start-x start-y))
 
 (defn draw [state]
   (q/background 0)
   (q/fill 255)
-  (q/text-size (:text-size state))
-  (q/text (str (:pressed-key state)) 20 20)
-  (q/text (:code state) 20 100)
-  (display-cursor state)
+  (doseq [code-unit (:code-list state)]
+    (display-code code-unit)
+    (display-cursor code-unit))
+  (q/text-size 20)
   (if (:pressing-ctr? state)
     (q/text "pressing ctr" 300 300))
   (if (:pressing-alt? state)
     (q/text "pressing alt" 300 350))
-  (q/text (str (:result state)) 300 400)
-  (q/text (str (:error state)) 300 450))
+  (q/text (str (:result (nth (:code-list state) (:code-unit-index state)))) 300 400)
+  (q/text (str (:error (nth (:code-list state) (:code-unit-index state)))) 300 500))
 
 
-(defn cursor-move-right [{:keys [cursor-index code] :as state}]
+(defn cursor-move-right [{:keys [cursor-index code] :as code-unit}]
   (if (< cursor-index (count code))
-    (assoc state :cursor-index (inc cursor-index))
-    state))
+    (assoc code-unit :cursor-index (inc cursor-index))
+    code-unit))
 
-(defn cursor-move-left [{:keys [cursor-index] :as state}]
+(defn cursor-move-left [{:keys [cursor-index] :as code-unit}]
   (if (not= cursor-index 0)
-    (assoc state :cursor-index (dec cursor-index))
-    state))
+    (assoc code-unit :cursor-index (dec cursor-index))
+    code-unit))
 
 (defn split-by-index
     [strg idx] [(subs strg 0 idx) (subs strg idx (count strg))])
 
-(defn insert-char [code char cursor-index]
+(defn insert-char [{:keys [code cursor-index] :as code-unit} char]
   (let [[left right] (split-by-index code cursor-index)]
-    (str left char right)))
+    (assoc code-unit :code (str left char right))))
 
-(defn delete-backward-char [{:keys [code cursor-index] :as state}]
+(defn delete-backward-char [{:keys [code cursor-index] :as code-unit}]
   (if (or (<= cursor-index 0) (> cursor-index (count code)))
-    state
-    (-> state
+    code-unit
+    (-> code-unit
         (assoc :code (str (subs code 0 (dec cursor-index))
                           (subs code cursor-index (count code))))
         (cursor-move-left))))
 
-(defn insert-paren [code cursor-index]
+(defn insert-paren [{:keys [code cursor-index] :as code-unit}]
   (let [[left right] (split-by-index code cursor-index)]
-    (str left "()" right)))
+    (assoc code-unit :code (str left "()" right))))
 
-(defn insert-bracket [code cursor-index]
+(defn insert-bracket [{:keys [code cursor-index] :as code-unit}]
   (let [[left right] (split-by-index code cursor-index)]
-    (str left "[]" right)))
+    (assoc code-unit :code (str left "[]" right))))
 
 (def special-key-code-dic (hash-map 8 :backspace
+                                    9 :tab
                                     10 :enter))
 
 (defn make-key-state []
@@ -131,42 +144,54 @@
 (defmulti key-pressed-functions (fn [key-state state]
                                   (:key-as-keyword-str key-state)))
 
-(defmethod key-pressed-functions ":(" [key-state state]
-  (play-chord-with-key state)
-  (-> state
-      (update-in [:chord-index] inc-chord-index)
-      (assoc :code (insert-paren (:code state) (:cursor-index state)))
-      (cursor-move-right)))
+(defn update-code [{:keys [code-list code-unit-index] :as state} f & arg]
+  (let [code-unit (nth code-list code-unit-index)]
+    (assoc state :code-list (assoc code-list code-unit-index (apply f code-unit arg)))))
 
-(defmethod key-pressed-functions ":[" [key-state state]
+(defmethod key-pressed-functions ":(" [key-state
+                                       {:keys [code-list code-unit-index] :as state}]
   (play-chord-with-key state)
   (-> state
       (update-in [:chord-index] inc-chord-index)
-      (assoc :code (insert-bracket (:code state) (:cursor-index state)))
-      (cursor-move-right)))
+      (update-code insert-paren)
+      (update-code cursor-move-right)))
 
-(defmethod key-pressed-functions ":[" [key-state state]
+(defmethod key-pressed-functions ":[" [key-state
+                                       {:keys [code-list code-unit-index] :as state}]
   (play-chord-with-key state)
   (-> state
       (update-in [:chord-index] inc-chord-index)
-      (assoc :code (insert-bracket (:code state) (:cursor-index state)))
-      (cursor-move-right)))
+      (update-code insert-bracket)
+      (update-code cursor-move-right)))
 
-(defmethod key-pressed-functions ": " [key-state state]
+(defmethod key-pressed-functions ": " [key-state
+                                       {:keys [code-list code-unit-index] :as state}]
   (play-chord-with-key state)
   (-> state
       (update-in [:chord-index] inc-chord-index)
-      (assoc  :code (insert-char (:code state) (q/raw-key) (:cursor-index state)))
-      (cursor-move-right)))
+      (update-code insert-char (q/raw-key))
+      (update-code cursor-move-right)))
 
 (defmethod key-pressed-functions ":shift" [key-state state]
   state)
 
-(defmethod key-pressed-functions ":right" [key-state state]
-  (cursor-move-right state))
+(defmethod key-pressed-functions ":right" [key-state
+                                           {:keys [code-list code-unit-index] :as state}]
+  (-> state
+      (update-code cursor-move-right)))
 
-(defmethod key-pressed-functions ":left" [key-state state]
-  (cursor-move-left state))
+(defmethod key-pressed-functions ":left" [key-state
+                                          {:keys [code-list code-unit-index] :as state}]
+  (-> state
+      (update-code cursor-move-left)))
+
+(defmethod key-pressed-functions ":up" [key-state
+                                        {:keys [code-list code-unit-index] :as state}]
+  (assoc state :code-unit-index (mod (dec code-unit-index) (count code-list))))
+
+(defmethod key-pressed-functions ":down" [key-state
+                                        {:keys [code-list code-unit-index] :as state}]
+  (assoc state :code-unit-index (mod (inc code-unit-index) (count code-list))))
 
 (defmethod key-pressed-functions ":control" [key-state state]
   (assoc state :pressing-ctr? true))
@@ -174,39 +199,52 @@
 (defmethod key-pressed-functions ":alt" [key-state state]
   (assoc state :pressing-alt? true))
 
-(defn delete-all [state]
-  (assoc state :code "" :cursor-index 0))
+(defn delete-all [code-unit]
+  (assoc code-unit :code "" :cursor-index 0))
 
-(defmethod key-pressed-functions ":backspace" [key-state state]
-  (println (:scale-index state))
+(defmethod key-pressed-functions ":backspace" [key-state
+                                               {:keys [code-list code-unit-index] :as state}]
   (if (:pressing-alt? state)
-    (delete-all state)
-    (do(play-with-key state)
-       (-> state
-           (update-in [:scale-index] inc-index)
-           (delete-backward-char)))))
+    (update-code state delete-all)
+    (do (play-with-key state)
+        (-> state
+            (update-in [:scale-index] inc-index)
+            (update-code delete-backward-char)))))
 
-(defn eval-code [state]
-  (try (let [sexp (read-string (:code state))
+(defn eval-code [code-unit]
+  (try (let [sexp (read-string (:code code-unit))
              result (binding [*ns* (find-ns 'parensymphony.core)] (eval sexp))]
-         (assoc state :result result :error ""))
-       (catch RuntimeException ex (assoc state
+         (assoc code-unit :result result :error ""))
+       (catch RuntimeException ex (assoc code-unit
                                          :result ""
                                          :error (.getMessage ex)))))
 
-(defmethod key-pressed-functions ":enter" [key-state state]
-  (if (:pressing-alt? state)
-    (eval-code state)
-    state))
+(defmethod key-pressed-functions ":enter" [key-state
+                                           {:keys [code-list code-unit-index] :as state}]
+  (let [code-unit (nth code-list code-unit-index)]
+    (if (:pressing-alt? state)
+      (update-code state eval-code)
+      (do (-> state
+              (update-code insert-char "\n")
+              (update-code cursor-move-right))))))
 
-(defmethod key-pressed-functions :default [key-state state]
-  (play-with-key state)
+(defmethod key-pressed-functions ":tab" [key-state
+                                           {:keys [code-list code-unit-index] :as state}]
   (-> state
-      (update-in [:scale-index] inc-index)
-      (assoc  :code (insert-char (:code state)
-                                 (q/raw-key)
-                                 (:cursor-index state)))
-      (cursor-move-right)))
+      (update-code insert-char "  ")
+      (update-code cursor-move-right)
+      (update-code cursor-move-right)))
+
+
+(defmethod key-pressed-functions :default [key-state
+                                           {:keys [code-list code-unit-index] :as state}]
+  (println (q/raw-key))
+  (let [code-unit (nth code-list code-unit-index)]
+    (play-with-key state)
+    (-> state
+        (update-in [:scale-index] inc-index)
+        (update-code insert-char (q/raw-key))
+        (update-code cursor-move-right))))
 
 (defmulti key-released-functions (fn [key-state state]
                                    (:key-as-keyword-str key-state)))
@@ -237,15 +275,16 @@
   (try (eval sexp)
        (catch RuntimeException ex nil)))
 
-(q/defsketch parensymphony
-  :title "symphony"
-  :setup setup
-  :draw draw
-  :key-pressed key-pressed
-  :key-released key-released
-  :renderer :p2d
-  :middleware [m/fun-mode m/pause-on-error]
-  :size [window-width window-height])
+(defn start []
+  (q/defsketch parensymphony
+    :title "symphony"
+    :setup setup
+    :draw draw
+    :key-pressed key-pressed
+    :key-released key-released
+    :renderer :p2d
+    :middleware [m/fun-mode m/pause-on-error]
+    :size [window-width window-height]))
 
 (def char-keycode-map
   (hash-map :0 48 :1 49 :2 50 :3 51 :4 52 :5 53 :6 54 :7 55 :8 56 :9 57
